94e1736dbc5384559fa8081355b5c448eb3656fa
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-09-25 05:06:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase === string) {
-    return "I can't hear you!"
-  } else if (string.toUpperCase === string ) {
-    return "YES INDEED!"
-  } else if (string === "I love you, Grandma.") new Promise(function(resolve, reject) {
-    return "I love you, too."
-  }
+    if (string.toLowerCase === string) {
+      return "I can't hear you!"
+    } else if (string.toUpperCase === string ) {
+      return "YES INDEED!"
+    } else if (string === "I love you, Grandma.") {
+      return "I love you, too."
+    }
   }
